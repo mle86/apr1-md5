fd11752015258dec349759c5c38aafc0d7d80af5
mle <maximilian@eul.cc>
2018-11-12 13:20:24 +0100

    test: also check mismatches

--- tests/APR1_MD5_CheckTest.php
+++ tests/APR1_MD5_CheckTest.php
@@ -6,10 +6,21 @@ class APR1_MD5_CheckTest extends PHPUnit_Framework_TestCase {
 
     public function knownHashMatches() {
         return array(
-            // syntax: array(knownPassword, knownHash)
-            array('WhiteHat101', '$apr1$HIcWIbgX$G9YqNkCVGlFAN63bClpoT/'),
-            array('apache',      '$apr1$rOioh4Wh$bVD3DRwksETubcpEH90ww0'),
-            array('ChangeMe1',   '$apr1$PVWlTz/5$SNkIVyogockgH65nMLn.W1'),
+            // syntax: array(knownHash, correctPassword)
+            array('$apr1$HIcWIbgX$G9YqNkCVGlFAN63bClpoT/', 'WhiteHat101'),
+            array('$apr1$rOioh4Wh$bVD3DRwksETubcpEH90ww0', 'apache'),
+            array('$apr1$PVWlTz/5$SNkIVyogockgH65nMLn.W1', 'ChangeMe1'),
+        );
+    }
+
+    public function knownHashMismatches() {
+        return array(
+            // syntax: array(knownHash, incorrectPassword)
+            array('$apr1$HIcWIbgX$G9YqNkCVGlFAN63bClpoT/', 'WhiteHat1011'),
+            array('$apr1$HIcWIbgX$G9YqNkCVGlFAN63bClpoT/', 'WhiteHat10x'),
+            array('$apr1$rOioh4Wh$bVD3DRwksETubcpEH90ww0', 'ap4che'),
+            array('$apr1$PVWlTz/5$SNkIVyogockgH65nMLn.W1', 'ChangeMe1' . "\x00"),
+            array('$apr1$PVWlTz/5$SNkIVyogockgH65nMLn.W1', 'ChangeMe2'),
         );
     }
 
@@ -19,6 +30,15 @@ class APR1_MD5_CheckTest extends PHPUnit_Framework_TestCase {
     public function testCheckHashMatch($knownHash, $correctPassword) {
         $this->assertTrue(
             APR1_MD5::check($correctPassword, $knownHash)
+        );
+    }
+
+    /**
+     * @dataProvider knownHashMismatches
+     */
+    public function testCheckHashMismatch($knownHash, $incorrectPassword) {
+        $this->assertFalse(
+            APR1_MD5::check($incorrectPassword, $knownHash)
         );
     }
 
