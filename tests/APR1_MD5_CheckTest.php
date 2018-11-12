a36d4ee3f8289cf11c7b7889079106c3c0855ac9
mle <maximilian@eul.cc>
2018-11-12 13:13:47 +0100

    test: cleanup test structure

--- tests/APR1_MD5_CheckTest.php
+++ tests/APR1_MD5_CheckTest.php
@@ -4,21 +4,21 @@ use WhiteHat101\Crypt\APR1_MD5;
 
 class APR1_MD5_CheckTest extends PHPUnit_Framework_TestCase {
 
-    public function testHash_WhiteHat101() {
-        $this->assertTrue(
-            APR1_MD5::check('WhiteHat101','$apr1$HIcWIbgX$G9YqNkCVGlFAN63bClpoT/')
+    public function knownHashMatches() {
+        return array(
+            // syntax: array(knownPassword, knownHash)
+            array('WhiteHat101', '$apr1$HIcWIbgX$G9YqNkCVGlFAN63bClpoT/'),
+            array('apache',      '$apr1$rOioh4Wh$bVD3DRwksETubcpEH90ww0'),
+            array('ChangeMe1',   '$apr1$PVWlTz/5$SNkIVyogockgH65nMLn.W1'),
         );
     }
 
-    public function testHash_apache() {
+    /**
+     * @dataProvider knownHashMatches
+     */
+    public function testCheckHashMatch($knownHash, $correctPassword) {
         $this->assertTrue(
-            APR1_MD5::check('apache','$apr1$rOioh4Wh$bVD3DRwksETubcpEH90ww0')
-        );
-    }
-
-    public function testHash_ChangeMe1() {
-        $this->assertTrue(
-            APR1_MD5::check('ChangeMe1','$apr1$PVWlTz/5$SNkIVyogockgH65nMLn.W1')
+            APR1_MD5::check($correctPassword, $knownHash)
         );
     }
 
--- tests/APR1_MD5_HashTest.php
+++ tests/APR1_MD5_HashTest.php
@@ -4,48 +4,32 @@ use WhiteHat101\Crypt\APR1_MD5;
 
 class APR1_MD5_HashTest extends PHPUnit_Framework_TestCase {
 
-    public function testHash_WhiteHat101() {
-        $this->assertEquals(
-            '$apr1$HIcWIbgX$G9YqNkCVGlFAN63bClpoT/',
-            APR1_MD5::hash('WhiteHat101','HIcWIbgX')
+    public function knownHashResults() {
+        return array(
+            // syntax: array(password, salt, expectedHashOutput)
+            array('WhiteHat101', 'HIcWIbgX', '$apr1$HIcWIbgX$G9YqNkCVGlFAN63bClpoT/'),
+            array('apache',      'rOioh4Wh', '$apr1$rOioh4Wh$bVD3DRwksETubcpEH90ww0'),
+            array('ChangeMe1',   'PVWlTz/5', '$apr1$PVWlTz/5$SNkIVyogockgH65nMLn.W1'),
+
+            // Test some awkward inputs:
+            array('ChangeMe1', '',                   '$apr1$$DbHa0iITto8vNFPlkQsBX1'),  // blank salt
+            array('ChangeMe1', 'PVWlTz/50123456789', '$apr1$PVWlTz/5$SNkIVyogockgH65nMLn.W1'),  // long salt
         );
     }
 
-    public function testHash_apache() {
+    /**
+     * @dataProvider knownHashResults
+     */
+    public function testHashResult($password, $salt, $expectedHashOutput) {
         $this->assertEquals(
-            '$apr1$rOioh4Wh$bVD3DRwksETubcpEH90ww0',
-            APR1_MD5::hash('apache','rOioh4Wh')
+            $expectedHashOutput,
+            APR1_MD5::hash($password,$salt)
         );
     }
 
-    public function testHash_ChangeMe1() {
-        $this->assertEquals(
-            '$apr1$PVWlTz/5$SNkIVyogockgH65nMLn.W1',
-            APR1_MD5::hash('ChangeMe1','PVWlTz/5')
-        );
-    }
-
-    // Test some awkward inputs
-
-    public function testHash_ChangeMe1_blankSalt() {
-        $this->assertEquals(
-            '$apr1$$DbHa0iITto8vNFPlkQsBX1',
-            APR1_MD5::hash('ChangeMe1','')
-        );
-    }
-
-    public function testHash_ChangeMe1_longSalt() {
-        $this->assertEquals(
-            '$apr1$PVWlTz/5$SNkIVyogockgH65nMLn.W1',
-            APR1_MD5::hash('ChangeMe1','PVWlTz/50123456789')
-        );
-    }
-
-    public function testHash_ChangeMe1_nullSalt() {
-        $hash = APR1_MD5::hash('ChangeMe1');
-        $this->assertEquals(37, strlen($hash));
-    }
-
+    /**
+     * @depends testHashResult
+     */
     public function testHash__nullSalt() {
         $hash = APR1_MD5::hash('');
         $this->assertEquals(37, strlen($hash));
@@ -53,6 +37,9 @@ class APR1_MD5_HashTest extends PHPUnit_Framework_TestCase {
 
     // a null password gets coerced into the blank string.
     // is this sensible?
+    /**
+     * @depends testHashResult
+     */
     public function testHash_null_nullSalt() {
         $hash = APR1_MD5::hash(null);
         $this->assertEquals(37, strlen($hash));
